963b536c4f7a2ab91c6e665c462219cd088f0d25
CaptainStabs <comportfailure@gmail.com>
2021-12-03 17:36:08 -0500

    add secrets and config templates

---
+++ _configs.py
@@ -0,0 +1,10 @@
+url = "https://codehs.com/student/xxxxxxx/section/xxxxxx/"
+
+configs = {
+    "student_number":"xxxxxxx",
+    "section_number":"yyyyyy",
+    "assignment_number":"cccccccc",
+    "end_number":"dddddddd",
+    "can_copy_paste": True,
+    "sign_in_with_google": True,
+}
